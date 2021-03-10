8178a32141cc5001484ad7af1f2108ccc23b6f05
zhaobowen <bowen.zhao@chiefclouds.com>
2021-03-10 17:20:30 +0800

    完成首页的布局

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,29 @@
       </div>
       <div class="icons">
         <div class="icons_item" v-for="(item,index) of list" v-bind:key="index">
-          <img class="icons_item_img" :src="item.src" alt="">
+          <img class="icons_item_img" :src="item.src">
           <p class="icons_item_desc">{{item.desc}}</p>
         </div>
       </div>
       <div class="gap"></div>
+      <div class="nearby">
+        <h3 class="nearby_title">附近店铺</h3>
+        <div class="nearby_item">
+          <img
+            src="http://www.dell-lee.com/imgs/vue3/near.png"
+            class="nearby_item_img"
+          >
+          <div class="nearby_content">
+            <div class="nearby_content_title">沃尔玛</div>
+            <div class="nearby_content_tags">
+              <span class="nearby_content_tag">月售一万+</span>
+              <span class="nearby_content_tag">月售一万+</span>
+              <span class="nearby_content_tag">月售一万+</span>
+            </div>
+            <p class="nearby_content_highlight">自尊VIP高亮-自尊VIP高亮-自尊VIP高亮-自尊VIP高亮</p>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="docker">
       <span class="docker_item docker_item-active">
@@ -87,7 +105,8 @@ export default {
   bottom: 0.5rem;
   right: 0;
   // background: rgb(221,153,169);
-  padding: 0 0.18rem;
+  padding: 0 0.18rem .1rem;;
+  overflow-y: auto;
 }
 .postion {
   position: relative;
@@ -155,6 +174,48 @@ export default {
   margin: 0 -0.18rem;
   height: .1rem;
   background: $content-bgcolor;
+}
+.nearby {
+  &_title {
+    margin: 0.16rem 0 0.02rem 0;
+    font-size: 0.18rem;
+    font-weight: normal;
+    color: $content-fontcolor;
+  }
+  &_item {
+    display: flex;
+    padding-top: .12rem;
+    &_img {
+      margin-right: .16rem;
+      width: .56rem;
+      height: .56rem;
+    }
+  }
+  &_content {
+    flex: 1;
+    padding-bottom: .12rem ;
+    border-bottom: 1px solid  $content-bgcolor;
+    &_title {
+      line-height: .22rem;
+      font-size: .16rem;
+      color: $content-fontcolor;
+    }
+    &_tags {
+      margin-top: 0.08rem;
+      line-height: .18rem;
+      font-size: .13rem;
+      color: $content-fontcolor;
+    }
+    &_tag {
+      margin-right: 0.16rem;
+    }
+    &_highlight {
+      margin: 0.08rem 0 0 0;
+      line-height: .18rem;
+      font-size: .13rem;
+      color: #e93b3b;
+    }
+  }
 }
 .docker {
   display: flex;
