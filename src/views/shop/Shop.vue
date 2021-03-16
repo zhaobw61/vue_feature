b2a5bfbd499c833c3f99464f8cda48a5a58d7078
zhaobowen <bowen.zhao@chiefclouds.com>
2021-03-16 15:06:42 +0800

    初始化shop的页面

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -1,25 +1,41 @@
 <template>
-  <div>
-      shop
+  <div class="wrap">
+      <ShopInfo
+      v-for="item in nearbyList"
+      :key = "item._id"
+      :item ="item"
+      :hideBorder="true"
+      ></ShopInfo>
   </div>
 </template>
 
 <script>
+import ShopInfo from '../../components/ShopInfo'
 export default {
-  data () {
-    return {
-
-    }
+  name: 'Shop',
+  components: {
+    ShopInfo
+  },
+  setup () {
+    const nearbyList = [{
+      expressLimit: 0,
+      expressPrice: 5,
+      imgUrl: 'http://www.dell-lee.com/imgs/vue3/near.png',
+      name: '沃尔玛',
+      sales: 10000,
+      slogan: 'VIP尊享满89元减4元运费券',
+      _id: '1'
+    }]
+    return { nearbyList }
   },
   methods: {
-
-  },
-  components: {
 
   }
 }
 </script>
 
 <style  scope lang='scss'>
-
+.wrap {
+padding: 0 0.18rem;
+}
 </style>
