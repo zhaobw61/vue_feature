a115fd53f497ce1f157296575abb481782e40936
zhaobowen <bowen.zhao@chiefclouds.com>
2021-03-17 16:36:42 +0800

    整理商家详情页面

--- src/views/shop/Shop.vue
+++ src/views/shop/Shop.vue
@@ -13,40 +13,48 @@
       </div>
     </div>
       <ShopInfo
-      v-for="item in data"
       :key = "item._id"
       :item ="item"
       :hideBorder="true"
+      v-if="item.imgUrl"
       ></ShopInfo>
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
-import { useRouter } from 'vue-router'
+import { reactive, toRefs } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import { get } from '../../utils/request'
 import ShopInfo from '../../components/ShopInfo'
+const useShopInfoEffect = () => {
+  const route = useRoute()
+  const data = reactive({ item: {} })
+  const getItemData = async () => {
+    const result = await get(`/api/shop/${route.params.name}`)
+    if (result.errno === 0 && result.data) {
+      data.item = result.data
+    }
+  }
+  const { item } = toRefs(data)
+  return { item, getItemData }
+}
+const useBackRouterEffect = () => {
+  const router = useRouter()
+  const handleBackClick = () => {
+    router.back()
+  }
+  return { handleBackClick }
+}
 export default {
   name: 'Shop',
   components: {
     ShopInfo
   },
   setup () {
-    const router = useRouter()
-    const data = reactive({
-      item: {}
-    })
-    const getItemData = async () => {
-      const result = await get('/api/shop/1')
-      if (result.errno === 0 && result.data) {
-        data.item = result.data
-      }
-    }
+    const { item, getItemData } = useShopInfoEffect()
+    const { handleBackClick } = useBackRouterEffect()
     getItemData()
-    const handleBackClick = () => {
-      router.back()
-    }
-    return { data, handleBackClick }
+    return { item, handleBackClick }
   }
 }
 </script>
